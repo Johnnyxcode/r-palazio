c6144331faa73c3d2aa6d8b02ef89b06bc1e9a0b
Johnnyxcode <john.ayo.jam@gmail.com>
2024-07-28 15:32:17 +0100

    swipe dowm modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { FaTimes } from "react-icons/fa"; // Import the FaTimes icon
+import { FaTimes } from "react-icons/fa";
+import { useDrag } from "@use-gesture/react";
 
 const Modal = ({ children, isOpen, onClose }) => {
   const [isVisible, setIsVisible] = useState(false);
@@ -8,33 +9,42 @@ const Modal = ({ children, isOpen, onClose }) => {
   useEffect(() => {
     if (isOpen) {
       setIsVisible(true);
-      document.body.style.overflow = "hidden"; // Prevent body scrolling when modal is open
+      document.body.style.overflow = "hidden";
     } else {
       setIsVisible(false);
-      document.body.style.overflow = "auto"; // Restore body scrolling when modal is closed
+      document.body.style.overflow = "auto";
     }
   }, [isOpen]);
 
   const handleClose = () => {
     setIsVisible(false);
-    setTimeout(onClose, 300); // Match this duration with your Tailwind transition duration
+    setTimeout(onClose, 300);
   };
+
+  const bind = useDrag(
+    ({ down, movement: [, my], direction: [, dy], velocity: [, vy] }) => {
+      if (!down && dy > 0 && vy > 0.2) {
+        handleClose();
+      }
+    }
+  );
 
   return (
     <>
       {isOpen && (
         <div className="fixed inset-0 z-50 flex items-end justify-center md:items-center">
           <div
-            className="fixed mt-40 inset-0 bg-black opacity-50"
+            className="fixed inset-0 bg-black opacity-50"
             onClick={handleClose}
           ></div>
           <div
+            {...bind()}
             className={`relative bg-white w-full md:w-[100%] rounded-lg shadow-lg p-8 z-50 transform transition-transform duration-300 ease-out ${
               isVisible ? "translate-y-0" : "translate-y-full"
             }`}
           >
             <button
-              className="absolute top-20 right-4 text-gray-600 hover:text-gray-900"
+              className="absolute top-10 right-4 text-gray-600 hover:text-gray-900"
               onClick={handleClose}
             >
               <FaTimes size={24} />
